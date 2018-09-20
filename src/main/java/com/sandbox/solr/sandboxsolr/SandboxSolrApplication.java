cdbdef42f037ef262cca1438a9d4c2b01817f9e6
robert <siara.robert@gmail.com>
2018-09-20 23:25:08 +0200

    init commit

---
+++ src/main/java/com/sandbox/solr/sandboxsolr/SandboxSolrApplication.java
@@ -0,0 +1,12 @@
+package com.sandbox.solr.sandboxsolr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSolrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxSolrApplication.class, args);
+    }
+}
